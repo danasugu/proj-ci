8d7fc6fbf185c543bb32a800d6ac2a57528c9976
Dana Sugu <dana.sugu@gmail.com>
2021-11-04 15:50:30 +0200

    Update header.php

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -1,3 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" >
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
