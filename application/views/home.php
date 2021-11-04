477084e4116afbd57233b95b5148f8b06cd6f17e
Dana Sugu <dana.sugu@gmail.com>
2021-11-04 15:38:10 +0200

    update

--- application/views/home.php
+++ application/views/home.php
@@ -10,7 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="<?= base_url(); ?>assets/css/bootstrapcss/bootstrap.min.css" rel="stylesheet" >
+    <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" >
 
     <title>Hello, world!</title>
   </head>
