dbffd041712f411835547d83aab51e8b5ab861fb
Dana Sugu <dana.sugu@gmail.com>
2021-11-04 15:52:12 +0200

    Update footer.php

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,3 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js" ></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    -->
+  </body>
+</html>
